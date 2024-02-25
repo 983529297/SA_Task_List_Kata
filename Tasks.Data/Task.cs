e0c9bd1b782c8638ebbfd0b8e9c2230ac19c7e2d
sam <sam22822964@gmail.com>
2024-02-25 22:08:16 +0800

    new tasks.data for taskList Data

---
+++ Tasks.Data/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks.Data
+{
+    class Task
+    {
+        public long Id { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
---
+++ Tasks.Data/TaskListData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tasks.Data
+{
+    public class TaskListData
+    {
+        private readonly IDictionary<string, IList<Task>> tasks = new Dictionary<string, IList<Task>>();
+
+    }
+}
